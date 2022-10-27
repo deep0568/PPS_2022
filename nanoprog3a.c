bb70dae6d2e34a826962aeabf4a4a9e29dfa1c75
deep0568 <116259208+deep0568@users.noreply.github.com>
2022-10-27 20:15:54 +0530

    Create nanoprog3a.c

---
+++ nanoprog3a.c
@@ -0,0 +1,18 @@
+## program1: write a code to display Dupinder kaur 2221039
+```
+#include<stdio.h>
+int main()
+{
+char firstname[20], lastname[20];
+int roll_no;
+printf("dupinder");
+scanf("%s", firstname);
+printf("kaur");
+scanf(%s, lastname);
+printf("20221039");
+scanf("%d",&roll_no);
+printf(%s %s %d\n" ,dupinder,kaur,20221039);
+return0;
+}
+```
+Output:Dupinder kaur 2221039**
