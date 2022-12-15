873fb232e1087ddd3e39ce5dc358e802041a5bdd
deep0568 <116259208+deep0568@users.noreply.github.com>
2022-12-15 21:34:55 +0530

    Prog6.c

---
+++ Prog6.c
@@ -0,0 +1,12 @@
+1 ## program 6: Write a program to store data in an array...
+2```c
+3 #include <stdio.h>
+4
+5 int main() {
+6   int i[]= {10,20,40,60};
+7   printf("%d" ,i[1]);
+8
+9  return 0;
+10 }
+11 
+12  output:20
